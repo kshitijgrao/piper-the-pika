39528a0c2dfa1bbb47cd9dcda8af03897da24036
s-ksrao <67560587+s-ksrao@users.noreply.github.com>
2022-11-15 09:05:27 -0800

    Initial Map-Window Testing
    Trying to figure out how to render only a certain section of a map using a large map with a smaller window.

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -5,13 +5,16 @@ class Game
 {
     public static readonly string Title = "Minimalist Game Framework";
     public static readonly Vector2 Resolution = new Vector2(640, 480);
+    Texture map = Engine.LoadTexture("map.jpg"); //1944 x 1172
+    public Vector2 window = new Vector2(640, 480);
 
     public Game()
     {
-        //testing commits
+
     }
 
     public void Update()
     {
+        Engine.DrawTexture(map, Vector2.Zero);
     }
 }
