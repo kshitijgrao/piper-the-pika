f3662d20af6f784b16ab1281631247070fac1980
s-ksrao <67560587+s-ksrao@users.noreply.github.com>
2022-11-16 08:31:09 -0800

    Added a scrolling window!

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -7,6 +7,7 @@ class Game
     public static readonly Vector2 Resolution = new Vector2(640, 480);
     Texture map = Engine.LoadTexture("map.jpg"); //1944 x 1172
     public Vector2 window = new Vector2(640, 480);
+    Vector2 pos = new Vector2(0, 0);
 
     public Game()
     {
@@ -15,6 +16,26 @@ class Game
 
     public void Update()
     {
-        Engine.DrawTexture(map, Vector2.Zero);
+        Engine.DrawTexture(map, pos);
+
+        if (Engine.GetKeyHeld(Key.Right) && pos.X > -1304)
+        {
+            pos.X -= Engine.TimeDelta * 200;
+        }
+
+        if (Engine.GetKeyHeld(Key.Left) && pos.X < 0)
+        {
+            pos.X += Engine.TimeDelta * 200;
+        }
+
+        if (Engine.GetKeyHeld(Key.Down) && pos.Y > -692)
+        {
+            pos.Y -= Engine.TimeDelta * 200;
+        }
+
+        if (Engine.GetKeyHeld(Key.Up) && pos.Y < 0)
+        {
+            pos.Y += Engine.TimeDelta * 200;
+        }
     }
 }
