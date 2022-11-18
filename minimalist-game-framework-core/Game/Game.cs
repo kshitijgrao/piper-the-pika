8d6d5229678a23ac4e8c699cb3abaa1e1ed68599
EdwardZ3723 <s-ezhou@lwsd.org>
2022-11-18 09:15:21 -0800

    moved scoreboard to separate class

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -5,35 +5,19 @@ class Game
 {
     public static readonly string Title = "Minimalist Game Framework";
     public static readonly Vector2 Resolution = new Vector2(640, 480);
+    Scoreboard sb;
 
-    //scoreboard elements
-    int score;
-    int time;
-    int flowers;
-    int extraLives;
 
     public Game()
     {
-
-        //scoreboard elements
-        score = 0;
-        time = 0;
-        flowers = 0;
-        extraLives = 1;
+        //scoreboard
+        sb = new Scoreboard();
     }
 
     public void Update()
     {
-        //Scoreboard.updateScoreboard();
+        sb.updateScoreboard();
 
-        //scoreboard elements
-        Engine.DrawString("SCORE: " + score, new Vector2(0,0), Color.Yellow, Engine.LoadFont("Arial.ttf", 40));
-        Engine.DrawString("TIME: " + time, new Vector2(0, 40), Color.Yellow, Engine.LoadFont("Arial.ttf", 40));
-        Engine.DrawString("RINGS: " + flowers, new Vector2(0, 80), Color.Yellow, Engine.LoadFont("Arial.ttf", 40));
-
-        //if collision with flower, increase
-        
-
-        score = flowers*10;
+    
     }
 }
--- minimalist-game-framework-core/Game/Scoreboard.cs
+++ minimalist-game-framework-core/Game/Scoreboard.cs
@@ -1,16 +1,44 @@
 ﻿using System;
-namespace Game.Game
-{
+
 	public class Scoreboard
 	{
-		public Scoreboard()
-		{
-		}
+	int score;
+	int time;
+	int flowers;
+    int extraLives;
 
-		public void updateScoreboard()
-		{
-
-		}
+    public Scoreboard()
+	{
+		score = 0;
+		time = 0;
+		flowers = 0;
+		extraLives = 1;
 	}
+
+	public void updateScoreboard()
+	{
+		Engine.DrawString("SCORE: " + score, new Vector2(0, 0), Color.Yellow, Engine.LoadFont("Arial.ttf", 40));
+		Engine.DrawString("TIME: " + time, new Vector2(0, 40), Color.Yellow, Engine.LoadFont("Arial.ttf", 40));
+		Engine.DrawString("RINGS: " + flowers, new Vector2(0, 80), Color.Yellow, Engine.LoadFont("Arial.ttf", 40));
+
+	}
+
+	public void updateScore()
+	{
+
+	}
+
+	public void updateTime()
+	{
+
+	}
+
+	public void updateFlowers()
+	{
+
+	}
+
+	
 }
+
 
