796bb89b0b22e1e7f6fc4af4216b1ce4660d2006
s-ksrao <67560587+s-ksrao@users.noreply.github.com>
2022-11-21 09:08:27 -0800

    Created my own class for Scrolling and Rendering

---
+++ minimalist-game-framework-core/Game/Rendering.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Rendering
+{
+    Texture map = Engine.LoadTexture("map.jpg"); //1944 x 1172
+    Vector2 pos = new Vector2(0, 0);
+
+
+    public Rendering()
+    {
+
+    }
+
+    public void scrollingWindow()
+    {
+        Engine.DrawTexture(map, pos);
+
+        if (Engine.GetKeyHeld(Key.Right) && pos.X > -1304)
+        {
+            pos.X -= Engine.TimeDelta * 200;
+        }
+
+        if (Engine.GetKeyHeld(Key.Left) && pos.X < 0)
+        {
+            pos.X += Engine.TimeDelta * 200;
+        }
+
+        if (Engine.GetKeyHeld(Key.Down) && pos.Y > -692)
+        {
+            pos.Y -= Engine.TimeDelta * 200;
+        }
+
+        if (Engine.GetKeyHeld(Key.Up) && pos.Y < 0)
+        {
+            pos.Y += Engine.TimeDelta * 200;
+
+        }
+
+    }
+
+}
