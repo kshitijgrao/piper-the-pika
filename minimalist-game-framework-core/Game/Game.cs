2dfac1f30cbdb306642e55cc032415adbc531f68
Yasemin Turkoglu <s-YTURKOGLU@lwsd.org>
2022-11-22 08:27:47 -0800

    Piper sprite map w/ dots

--- minimalist-game-framework-core/Game/Game.cs
+++ minimalist-game-framework-core/Game/Game.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 class Game
 {
-    public static readonly string Title = "Minimalist Game Framework";
+    public static readonly string Title = "Piper the Pika";
     public static readonly Vector2 Resolution = new Vector2(640, 480);
 
     public Game()
