5a73e9f84d1e6ff5eff7c07d6e29dfcfba301ef7
EdwardZ3723 <s-ezhou@lwsd.org>
2022-11-22 09:01:07 -0800

    Fixed font memory usage issue

--- minimalist-game-framework-core/Game/Scoreboard.cs
+++ minimalist-game-framework-core/Game/Scoreboard.cs
@@ -7,47 +7,69 @@
 	int flowers;
     int extraLives;
 	Texture pikaPlaceholder = Engine.LoadTexture("pikaPlaceholder.png");
+	Font arial = Engine.LoadFont("Arial.ttf", 10);
 
 		public Scoreboard()
 		{
 			score = 0;
 			time = 0;
 			flowers = 0;
-			extraLives = 1;
+			extraLives = 3;
 		}
 		
 		public void updateScoreboard()
 		{
+			//check for game over
+			if (extraLives == 0)
+			{
+				gameOver();
+			}
 			time += Engine.TimeDelta;
 
-			//topLeft elements
-			Engine.DrawString("SCORE", new Vector2(0, 0), Color.Yellow, Engine.LoadFont("Arial.ttf", 10));
-			Engine.DrawString("" + score, new Vector2(40, 0), Color.White, Engine.LoadFont("Arial.ttf", 10));
-
-			Engine.DrawString("TIME", new Vector2(0, 15), Color.Yellow, Engine.LoadFont("Arial.ttf", 10));
-	        Engine.DrawString(""+(int)time, new Vector2(28, 15), Color.White, Engine.LoadFont("Arial.ttf", 10));
-
-			Engine.DrawString("RINGS", new Vector2(0, 30), Color.Yellow, Engine.LoadFont("Arial.ttf", 10));
-			Engine.DrawString("" + flowers, new Vector2(35, 30), Color.White, Engine.LoadFont("Arial.ttf", 10));
+			//topLeft scoreboard elements
 
 
+			Engine.DrawString("SCORE", new Vector2(0, 0), Color.Yellow, arial);
+			Engine.DrawString("" + score, new Vector2(40, 0), Color.White, arial);
+			Engine.DrawString("TIME", new Vector2(0, 15), Color.Yellow, arial);
+	        Engine.DrawString(""+(int)time, new Vector2(28, 15), Color.White, arial);
+			Engine.DrawString("RINGS", new Vector2(0, 30), Color.Yellow, arial);
+			Engine.DrawString("" + flowers, new Vector2(35, 30), Color.White, arial);
 
-			//bottom left elements
+
+
+			//bottom left scoreboard elements
 			Engine.DrawTexture(pikaPlaceholder, new Vector2(6, 197), size: new Vector2(19, 17));
-			Engine.DrawString("PIPER", new Vector2(26, 195), Color.Yellow, Engine.LoadFont("Arial.ttf", 10));
-			Engine.DrawString("x      " + extraLives, new Vector2(26, 204), Color.White, Engine.LoadFont("Arial.ttf", 10));
-	
+			Engine.DrawString("PIPER", new Vector2(26, 195), Color.Yellow, arial);
+			Engine.DrawString("x      " + extraLives, new Vector2(26, 204), Color.White, arial);
 		}
 
 
-
-		public void addFlowers(int x) 
+		//modify flower count
+		public void modifyFlowers(int x) 
 		{
 			flowers += x;
-			score += x * 10;
+			modifyScore(x * 10);
 		}
 
-	
+
+		//if an enemy is killed, add 100 to score
+		public void modifyScore(int x)
+		{
+			score += x;
+		}
+
+		//input -1 if a life is lost
+		//input 1 if an extra life is gained
+		public void modifyLives(int x)
+		{
+			extraLives += x;
+		}
+
+		public void gameOver()
+		{
+			
+		}	
 }
 
 
