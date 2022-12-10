5a65ed1969b895d066161e23748f78442e96b873
EdwardZ3723 <s-ezhou@lwsd.org>
2022-12-09 16:16:36 -0800

    added blinking rate in titleSccene

--- minimalist-game-framework-core/Game/Cutscenes.cs
+++ minimalist-game-framework-core/Game/Cutscenes.cs
@@ -2,7 +2,7 @@
 
 public class Scenes
 	{
-
+		//fonts
         static Font h1 = Engine.LoadFont("Arial.ttf", 40);
 		static Font h2 = Engine.LoadFont("Arial.ttf", 20);
 		static Font h3 = Engine.LoadFont("Arial.ttf", 12);
@@ -26,11 +26,11 @@ public class Scenes
 		{
 			return false;
 		}
+
         Engine.DrawTexture(piper, new Vector2(140, 50), size: new Vector2(50, 50));
         Engine.DrawString("PIPER", new Vector2(100, 100), Color.White, h1);
         Engine.DrawString("THE PIKA", new Vector2(115, 140), Color.White, h2);
-
-		if (frameCount%60<=30)
+		if (frameCount%90<=45)
 		{
 			Engine.DrawString("Click to Start", new Vector2(125, 170), Color.Yellow, h3);
 		}
