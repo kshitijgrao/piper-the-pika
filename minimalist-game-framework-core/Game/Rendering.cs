59e2f9a99154ade1a1971f06bcbbec9df24a6ecb
s-ksrao <67560587+s-ksrao@users.noreply.github.com>
2023-01-13 23:59:41 -0800

    Added some new images to make the map more seamless.

--- minimalist-game-framework-core/Game/Rendering.cs
+++ minimalist-game-framework-core/Game/Rendering.cs
@@ -21,7 +21,7 @@ class Rendering
 
     public void scrollingMotion()
     {
-        Engine.DrawTexture(bg, (pos / 2) - new Vector2(0, 420));
+        Engine.DrawTexture(bg, (pos / 2) - new Vector2(2382, 420));
         Engine.DrawTexture(map, pos);
 
         foreach (Flower flow in Game.flowers)
